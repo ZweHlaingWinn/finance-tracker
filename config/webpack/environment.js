4d94b3f0e33dec4b6b281dee7b565ee4e5e660c2
Zwe Hlaing Win <zwehlaingwin@zwenex.com>
2020-05-06 03:12:11 +0630

    Add iex api

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = requier("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+
+    $: 'jquery',
+    
+    jQuery: 'jquery',
+    
+    Popper: ['popper.js', 'default']
+    
+    }))
+
 module.exports = environment
